7f95718398e4095f6cfad60d1f7ea031d78204e1
PatrickStarM <625790276@qq.com>
2022-09-27 15:47:43 +0800

    feat: 微服务网关

---
+++ gateway-service/src/main/java/com/mth/gateway/GatewayServiceApplication.java
@@ -0,0 +1,16 @@
+package com.mth.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: mth
+ * @date: 2022/9/27
+ **/
+@SpringBootApplication
+public class GatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class);
+    }
+}
